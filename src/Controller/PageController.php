d857b3d5448867f8d3af347169325d365b01ceb0
Sandrine Orieux <sandrine.orieux@sfr.fr>
2024-02-07 17:38:45 +0100

    modification of footer and create logo partial

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RepairServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(RepairServiceRepository $repoService): Response
     {
-        return $this->render('home/index.html.twig', []);
+        $repairServices = $repoService->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'repairService' => $repairServices
+        ]);
     }
 
     #[Route('/contact', name: 'app_contact')]
